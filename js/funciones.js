2fbfa0ac04d5ae09e9d05278d143f0991a0ade8d
camilo-1822@hotmail.com <camilo1822@gmail.com>
2017-04-16 21:52:54 -0500

    detalles de lugares

--- js/funciones.js
+++ js/funciones.js
@@ -14,10 +14,15 @@ function onReadyPortal(__jquery) {
             __jquery("#bodyLogin").css("display","none");
          } 
         }
+        if (__jquery("#detalles").length) {
+            var x = obtenerIdUrl("id");
+            lugar(x);
+        }
 
 }
 
 function listarLugares(){
+    __jquery( ".listaLugares div" ).remove();
     __jquery.ajax({
         type: "GET",
         url: "https://cultural-api.herokuapp.com/api/Lugares",
@@ -25,7 +30,7 @@ function listarLugares(){
         dataType: "json",
         success: function(data){
            for(var i = 0; i < data.length;i++){
-                    var ht = "<div class=\"lugar\"><input type=\"button\" id=\"iconLugar\" class=\"iconLugar\" onclick=\"eliminar(\'"+data[i]._id+"\')\"/><div>"+data[i].title+"</div><div>"+data[i].description+"</div></div>"
+                    var ht = "<div class=\"lugar\"><input type=\"button\" id=\"iconLugar\" class=\"iconLugar\" onclick=\"eliminar(\'"+data[i]._id+"\')\"/><div onclick=\"mirar(\'"+data[i]._id+"\')\">"+data[i].title+"</div><div onclick=\"mirar(\'"+data[i]._id+"\')\">"+data[i].description+"</div></div>"
                     __jquery( ".listaLugares" ).append(ht);
                 }
         }
@@ -71,7 +76,6 @@ function agregarLugar(){
     document.getElementById("descripcion").value= "";
     document.getElementById("imagen").value= "";
     document.getElementById("direccion").value= "";
-    document.getElementById("tipo").value= "";
     document.getElementById("lat").value= "";
     document.getElementById("lon").value= "";
 }
@@ -97,12 +101,54 @@ function eliminar(x){
     var base='https://cultural-api.herokuapp.com/api/Lugares/'+x;
     __jquery.ajax({
         type: "delete",
-        method : 'delete',
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
         url : base,
         success:  function () {
-             alert("ya");
+            location.reload();
+        }
+    });
+}
+
+function eliminar1(x){
+    var base='https://cultural-api.herokuapp.com/api/Lugares/'+x;
+    __jquery.ajax({
+        type: "delete",
+        url : base,
+        success:  function () {
+            window.location.href = "index.html";
+        }
+    });
+}
+
+function mirar(x){
+    window.location.href = "detalle.html?id=" + x;
+}
+
+function obtenerIdUrl(name) {
+    try {
+        var regexS = "[\\?&]" + name + "=([^&#]*)";
+        var regex = new RegExp(regexS);   
+        var results = regex.exec(window.location.href);    
+        if (results == null) {
+            return "";
+        } else {
+            return results[1];
+        }
+    } catch (excepcion) {
+        dispararExcepcion(excepcion, "interna.min.js", "obtenerIdUrl()");
+    }
+}
+
+function lugar(x){
+    __jquery.ajax({
+        type: "GET",
+        url: "https://cultural-api.herokuapp.com/api/Lugares/"+x,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(data){
+            __jquery( "#tituloLg" ).text(data.title);
+            __jquery( "#descripcionLg" ).text(data.description);
+            __jquery( "#imagenLg" ).append("<img id=\"imgLg\" src=\""+data.image+"\" />");
+             __jquery( "#botonLg" ).append("<input type=\"button\" value=\"Eliminar\" onclick=\"eliminar1(\'"+data._id+"\')\"/>");
         }
     });
 }
